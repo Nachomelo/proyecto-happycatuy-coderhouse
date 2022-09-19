ca63d395d6010216b0cf2a328d9cca9858736540
NachoMelo <nachomelo_1@hotmail.com>
2022-09-19 09:47:12 -0300

    entrega final JavaScript

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const convert = document.getElementById('btnConvert')
 const inputFrom = document.getElementById('currencyFrom')
 const inputTo = document.getElementById('currencyTo')
 const convertedBreakdown = document.getElementById ('convertedBreakdown')
+const saveTaxes= document.getElementById('saveTaxes')
 
 let cicles = 0
 let precioIngresado = 0
@@ -27,212 +28,10 @@ let netValue = 0
 let totalExclusiveCiclo = 0
 let preTotal =0
 let totalExclusiveTotal=0
-
+let total=[]
 
 let remove = 0
 
-// Creacion de Taxes 
-taxform.addEventListener('submit',(event)=> {
-    event.preventDefault()
-    
-    const name = inputname.value
-    const type = inputtype.value
-    const value = inputvalue.value
-    const freq = inputfreq.value
-
-    const tax ={
-        name, type, value, freq
-    }
-    if (tax.value>=0.1){
-        taxes.push (tax)
-    const tr = document.createElement('tr')
-    tr.innerHTML=`
-                <tr>
-                <td id="tableElement"> ${tax.name}  </td>
-                <td id="tableElement"> ${tax.type}  </td>
-                <td id="tableElement"> ${tax.value} </td>
-                <td id="tableElement"> ${tax.freq} </td>
-                <button type="button" class="btnRemove btn btn-outline-danger btn-sm" >X</button>
-                </tr>
-    `
-    createdTaxes.append(tr)   
-  
-    taxform.reset()
-
-    const taxesjson = JSON.stringify (taxes)
-    localStorage.setItem("taxes",taxesjson)
-        inputname.classList.remove ("failure")
-        inputvalue.classList.remove ("failure")
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Falta informacion para crear el impuesto!',
-            footer: '<a href="">Es necesario ingresar un nombre y el valor porcentual del mismo para crearlo</a>',
-            confirmButtonColor: "#635959",
-            background: 'white'
-          })
-          const buttonOK = document.getElementsByClassName('swal2-confirm')
-          console.log (buttonOK)
-          buttonOK.classList.remove("swal2-styled")
-          buttonOK.classList.add("btn btn-secondary")
-
-        inputname.classList.add ("failure")
-        inputvalue.classList.add ("failure")
-    }
-    
-// Remover Taxes 
-
-    const removeTax = document.getElementsByClassName('btnRemove')
-    for ( let i=0; i < removeTax.length; i++){
-        let buttonT = removeTax[i]
-        buttonT.addEventListener ('click', function(event){
-            let buttonClicked = event.target
-            buttonClicked.parentElement.remove()
-            removeTaxf(i)            
-        })
-    }
-
-});
-
-
-///CALCULO -------------------------------------------------
-
-priceForm.addEventListener ('submit', (event)=>{
-    event.preventDefault()
-    priceForm.addEventListener ('submit', (event)=> {document.getElementById("btnCalculate").disabled = false})
-    precioIngresado = Number(inputPrice.value)
-    cicles = Number(inputCicle.value)
-    if (cicles<1) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'La cantidad deciclos tiene que ser mayor o igual a 1!',
-            footer: '<a href="">Corrije esta informacion y presiona cargar nuevamente</a>',
-            confirmButtonColor: "#635959",
-            background: 'white'
-          })
-    }
-    const liBreakdown = document.querySelectorAll ('.breakdownhtml')
-    liBreakdown.forEach (breakdownhtml => {breakdownhtml.remove()})
-    
-})  
-
-calculate.addEventListener ('click', (event)=> {
-    if (precioIngresado<=1){
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Falta informacion para realizar el calculo!',
-            footer: '<a href="">Es necesario cargar el precio ingresado utilizando la opcion "Cargar" para luego poder realizar el calculo</a>',
-            confirmButtonColor: "#635959",
-            background: 'white'
-          })
-    } 
-
-    const ciclo = taxes.filter ((el) => el.freq.includes ('ciclo'))
-    const total = taxes.filter ((el) => el.freq.includes ('total'))
-    
-    // CALCULO CICLO ---------------------------------------------------------
-    const inclusiveTaxesCiclo = ciclo.filter ((el) => el.type.includes ('inclusivo'))
-    const totalInclusiveCiclo = inclusiveTaxesCiclo.reduce ((acc, el) => acc + Number(el.value), 0)  
-    const exclusiveTaxesCiclo = ciclo.filter ((el) => el.type.includes ('exclusivo'))
-    calculatedExclusiveCiclo = exclusiveTaxesCiclo.map(({name, value})=>{
-        return {name, value:(value *precioIngresado)/100}})
-        
-    netValue = precioIngresado/(1+(totalInclusiveCiclo/100))
-
-    calculatedInclusiveCiclo = inclusiveTaxesCiclo.map(({name, value})=>{
-        return {name, value:(value/100)* netValue}})
-
-
-    if (exclusiveTaxesCiclo.length>=1){totalExclusiveCiclo=(calculatedExclusiveCiclo.reduce ((acc, el) => acc + el.value , 0)+precioIngresado)} 
-    else {totalExclusiveCiclo = (precioIngresado*cicles)}
-
-    const printIncCiclo = []
-
-    for (const elem of calculatedInclusiveCiclo) {
-        printIncCiclo.push (elem.name+" "+elem.value) 
-    }   
-
-    const printExcCiclo = []
-
-    for (const elem of calculatedExclusiveCiclo) {
-        printExcCiclo.push (elem.name+" "+elem.value)  
-
-
-    // CALCULO TOTAL
-
-    preTotal = (totalExclusiveCiclo*cicles)
-    const inclusiveTaxesTotal = total.filter ((el) => el.type.includes ('inclusivo'))
-    const totalInclusiveTotal = inclusiveTaxesTotal.reduce ((acc, el) => acc + Number(el.value), 0)
-    const exclusiveTaxesTotal = total.filter ((el) => el.type.includes ('exclusivo'))
-    calculatedExclusiveTotal = exclusiveTaxesTotal.map(({name, value})=>{
-        return {name, value:(value *preTotal)/100}})
-        
-    let netValueTotal = preTotal/(1+(totalInclusiveTotal/100))
-
-    calculatedInclusiveTotal = inclusiveTaxesTotal.map(({name, value})=>{
-        return {name, value:(value/100)* netValueTotal}})
-        totalExclusiveTotal = (calculatedExclusiveTotal.reduce ((acc, el) => acc + el.value , 0)+preTotal)
-        
-
-    const printIncTotal = []
-
-    for (const elem of calculatedInclusiveTotal) {
-        printIncTotal.push (elem.name+" "+elem.value) 
-    }   
-
-    const printExcTotal = []
-
-    for (const elem of calculatedExclusiveTotal) {
-        printExcTotal.push (elem.name+" "+elem.value)  
-    }
-
-    // Mostrar el breakdown //       
-    
-    if (total.length>= 1) {
-
-        const breakdownCiclo = document.createElement ('breakdownCiclo')
-
-        breakdownCiclo.innerHTML=`
-        <li class="breakdownhtml"> Precio neto:  ${netValue}</li>
-        <li class="breakdownhtml"> ${printIncCiclo} </li>
-        <li class="breakdownhtml"> ${printExcCiclo} </li>
-        <li class="breakdownhtml"> Total por Periodo:  ${totalExclusiveCiclo} </li>
-        <li class="breakdownhtml"> Subtotal:  ${preTotal}</li>
-        `
-        CalculatedBreakdown.append(breakdownCiclo)
-    
-        const breakdownTotal = document.createElement ('breakdownTotal')
-    
-        breakdownTotal.innerHTML=`
-        <li class="breakdownhtml"> ${printIncTotal} </li>
-        <li class="breakdownhtml"> ${printExcTotal} </li>
-        <li class="breakdownhtml"> Total:  ${totalExclusiveTotal} </li>
-        `
-        CalculatedBreakdown.append(breakdownTotal)
-
-    } else {
-    const breakdownCiclo = document.createElement ('breakdownCiclo')
-
-    breakdownCiclo.innerHTML=`
-    <li class="breakdownhtml"> Precio neto:  ${netValue}</li>
-    <li class="breakdownhtml"> ${printIncCiclo} </li>
-    <li class="breakdownhtml"> ${printExcCiclo} </li>
-    <li class="breakdownhtml"> Total por Periodo:  ${totalExclusiveCiclo} </li>
-    <li class="breakdownhtml"> Total  ${preTotal}</li>
-    `
-    CalculatedBreakdown.append(breakdownCiclo)
-
-    }
-    priceForm.addEventListener ('click', (event)=> {document.getElementById("btnCalculate").disabled = true})
-}
-})
-
-function removeTaxf (element) {
-    taxes.splice(element, 1)
-}
 
 // Selectores de Currencies
 
@@ -279,6 +78,215 @@ fetch('https://currency-exchange.p.rapidapi.com/listquotes', options)
     })
 	.catch(err => console.error(err));
 
+// Creacion de Taxes 
+taxform.addEventListener('submit',(event)=> {
+    event.preventDefault()
+    
+    const name = inputname.value
+    const type = inputtype.value
+    const value = inputvalue.value
+    const freq = inputfreq.value
+
+    const tax ={
+        name, type, value, freq
+    }
+
+    if (tax.value>=0.1){
+        taxes.push (tax)
+    const tr = document.createElement('tr')
+    tr.innerHTML=`
+                <tr>
+                <td id="tableElement"> ${tax.name}  </td>
+                <td id="tableElement"> ${tax.type}  </td>
+                <td id="tableElement"> ${tax.value} </td>
+                <td id="tableElement"> ${tax.freq} </td>
+                <button type="button" class="btnRemove btn btn-outline-danger btn-sm" >X</button>
+                </tr>
+    `
+    createdTaxes.append(tr)   
+  
+    taxform.reset()
+
+        inputname.classList.remove ("failure")
+        inputvalue.classList.remove ("failure")
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Falta informacion para crear el impuesto!',
+            footer: '<a href="">Es necesario ingresar un nombre y el valor porcentual del mismo para crearlo</a>',
+            confirmButtonColor: "#635959",
+            background: 'white'
+          })
+          const buttonOK = document.getElementsByClassName('swal2-confirm')
+          console.log (buttonOK)
+          buttonOK.classList.remove("swal2-styled")
+          buttonOK.classList.add("btn btn-secondary")
+
+        inputname.classList.add ("failure")
+        inputvalue.classList.add ("failure")
+    }
+    
+// Remover Taxes 
+
+    const removeTax = document.getElementsByClassName('btnRemove')
+    for ( let i=0; i < removeTax.length; i++){
+        let buttonT = removeTax[i]
+        buttonT.addEventListener ('click', function(event){
+            let buttonClicked = event.target
+            buttonClicked.parentElement.remove()
+            removeTaxf(i)            
+        })
+    }
+
+});
+
+///Cargar Datos -------------------------------------------------
+
+priceForm.addEventListener ('submit', (event)=>{
+    event.preventDefault()
+    priceForm.addEventListener ('submit', (event)=> {document.getElementById("btnCalculate").disabled = false})
+    precioIngresado = Number(inputPrice.value)
+    cicles = Number(inputCicle.value)
+    if (cicles<1) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'La cantidad deciclos tiene que ser mayor o igual a 1!',
+            footer: '<a href="">Corrije esta informacion y presiona cargar nuevamente</a>',
+            confirmButtonColor: "#635959",
+            background: 'white'
+          })
+    }
+    const liBreakdown = document.querySelectorAll ('.breakdownhtml')
+    liBreakdown.forEach (breakdownhtml => {breakdownhtml.remove()})
+    
+})  
+
+// CALCULO ------------------
+
+calculate.addEventListener ('click', (event)=> {
+    if (precioIngresado<=1){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Falta informacion para realizar el calculo!',
+            footer: '<a href="">Es necesario cargar el precio ingresado utilizando la opcion "Cargar" para luego poder realizar el calculo</a>',
+            confirmButtonColor: "#635959",
+            background: 'white'
+          })
+    } 
+
+    const ciclo = taxes.filter ((el) => el.freq.includes ('ciclo'))
+    total = taxes.filter ((el) => el.freq.includes ('total'))
+    
+    // CALCULO CICLO ---------------------------------------------------------
+    
+    const inclusiveTaxesCiclo = ciclo.filter ((el) => el.type.includes ('inclusivo'))
+    const totalInclusiveCiclo = inclusiveTaxesCiclo.reduce ((acc, el) => acc + Number(el.value), 0)  
+    const exclusiveTaxesCiclo = ciclo.filter ((el) => el.type.includes ('exclusivo'))
+    calculatedExclusiveCiclo = exclusiveTaxesCiclo.map(({name, value})=>{
+        return {name, value:(value *precioIngresado)/100}})
+        
+    netValue = precioIngresado/(1+(totalInclusiveCiclo/100))
+
+    calculatedInclusiveCiclo = inclusiveTaxesCiclo.map(({name, value})=>{
+        return {name, value:(value/100)* netValue}})
+
+
+    if (exclusiveTaxesCiclo.length>=1){totalExclusiveCiclo=(calculatedExclusiveCiclo.reduce ((acc, el) => acc + el.value , 0)+precioIngresado)} 
+    else {totalExclusiveCiclo = (precioIngresado*cicles)}
+
+    const printIncCiclo = []
+
+    for (const elem of calculatedInclusiveCiclo) {
+        printIncCiclo.push (elem.name+" "+elem.value) 
+    }   
+    
+    const printExcCiclo = []
+
+    for (const elem of calculatedExclusiveCiclo) {
+        printExcCiclo.push (elem.name+" "+elem.value)
+    }
+   
+
+    // CALCULO TOTAL
+
+    preTotal = (totalExclusiveCiclo*cicles)
+    const inclusiveTaxesTotal = total.filter ((el) => el.type.includes ('inclusivo'))
+    const totalInclusiveTotal = inclusiveTaxesTotal.reduce ((acc, el) => acc + Number(el.value), 0)
+    const exclusiveTaxesTotal = total.filter ((el) => el.type.includes ('exclusivo'))
+    calculatedExclusiveTotal = exclusiveTaxesTotal.map(({name, value})=>{
+        return {name, value:(value *preTotal)/100}})
+        
+    let netValueTotal = preTotal/(1+(totalInclusiveTotal/100))
+
+    calculatedInclusiveTotal = inclusiveTaxesTotal.map(({name, value})=>{
+        return {name, value:(value/100)* netValueTotal}})
+        totalExclusiveTotal = (calculatedExclusiveTotal.reduce ((acc, el) => acc + el.value , 0)+preTotal)
+        
+
+    const printIncTotal = []
+
+    for (const elem of calculatedInclusiveTotal) {
+        printIncTotal.push (elem.name+" "+elem.value) 
+    }   
+    
+
+    const printExcTotal = []
+
+    for (const elem of calculatedExclusiveTotal) {
+        printExcTotal.push (elem.name+" "+elem.value)  
+    }
+
+
+    // Mostrar el breakdown //       
+    
+    if (total.length>= 1) {
+
+        const breakdownCiclo = document.createElement ('breakdownCiclo')
+
+        breakdownCiclo.innerHTML=`
+        <li class="breakdownhtml"> Precio neto:  ${netValue}</li>
+        <li class="breakdownhtml"> ${printIncCiclo} </li>
+        <li class="breakdownhtml"> ${printExcCiclo} </li>
+        <li class="breakdownhtml"> Total por Periodo:  ${totalExclusiveCiclo} </li>
+        <li class="breakdownhtml"> Subtotal:  ${preTotal}</li>
+        `
+        CalculatedBreakdown.append(breakdownCiclo)
+    
+        const breakdownTotal = document.createElement ('breakdownTotal')
+    
+        breakdownTotal.innerHTML=`
+        <li class="breakdownhtml"> ${printIncTotal} </li>
+        <li class="breakdownhtml"> ${printExcTotal} </li>
+        <li class="breakdownhtml"> Total:  ${totalExclusiveTotal} </li>
+        `
+        CalculatedBreakdown.append(breakdownTotal)
+
+    } else {
+    const breakdownCiclo = document.createElement ('breakdownCiclo')
+
+    breakdownCiclo.innerHTML=`
+    <li class="breakdownhtml"> Precio neto:  ${netValue}</li>
+    <li class="breakdownhtml"> ${printIncCiclo} </li>
+    <li class="breakdownhtml"> ${printExcCiclo} </li>
+    <li class="breakdownhtml"> Total por Periodo:  ${totalExclusiveCiclo} </li>
+    <li class="breakdownhtml"> Total  ${preTotal}</li>
+    `
+    CalculatedBreakdown.append(breakdownCiclo)
+
+    }
+    priceForm.addEventListener ('click', (event)=> {document.getElementById("btnCalculate").disabled = true})
+
+
+function removeTaxf (element) {
+    taxes.splice(element, 1)
+}
+
+
+
+
 
 // Convert //
 
@@ -297,6 +305,8 @@ convert.addEventListener ('click',(event)=> {
 	.then(res => res.json())
 	.then(res => {
         const convPrecio= (precioIngresado*res)
+
+
 
     // CONVERTIR VALORES CICLO 
 
@@ -335,30 +345,55 @@ convert.addEventListener ('click',(event)=> {
                 printConvIncTotal.push (elem.name+" "+elem.value) 
             } 
             console.log (printConvIncTotal)  
+
     // Mostrar el breakdown convertido//  
 
-    const breakdownCicloConv = document.createElement ('breakdownCicloConv')
+    if (total.length>=1){
+        const breakdownCicloConv = document.createElement ('breakdownCicloConv')
 
         breakdownCicloConv.innerHTML=`
-        <li class="breakdownhtml"> Precio neto:  ${(netValue*res)}</li>
-        <li class="breakdownhtml"> ${printConvIncCiclo} </li>
-        <li class="breakdownhtml"> ${printConvExcCiclo} </li>
-        <li class="breakdownhtml"> Total por Periodo:  ${(totalExclusiveCiclo*res)} </li>
-        <li class="breakdownhtml"> Subtotal:  ${preTotal}</li>
+        <li class="breakdownhtmlConv"> Precio neto:  ${(netValue*res)}</li>
+        <li class="breakdownhtmlConv"> ${printConvIncCiclo} </li>
+        <li class="breakdownhtmlConv"> ${printConvExcCiclo} </li>
+        <li class="breakdownhtmlConv"> Total por Periodo:  ${(totalExclusiveCiclo*res)} </li>
+        <li class="breakdownhtmlConv"> Subtotal:  ${preTotal}</li>
         `
         convertedBreakdown.append(breakdownCicloConv)
 
         const breakdownTotalConv = document.createElement ('breakdownTotalConv')
     
         breakdownTotalConv.innerHTML=`
-        <li class="breakdownhtml"> ${printConvIncTotal} </li>
-        <li class="breakdownhtml"> ${printConvExcTotal} </li>
-        <li class="breakdownhtml"> Total:  ${(totalExclusiveTotal*res)} </li>
+        <li class="breakdownhtmlConv"> ${printConvIncTotal} </li>
+        <li class="breakdownhtmlConv"> ${printConvExcTotal} </li>
+        <li class="breakdownhtmlConv"> Total:  ${(totalExclusiveTotal*res)} </li>
         `
         convertedBreakdown.append(breakdownTotalConv)
 
+    }else{
+        const breakdownCicloConv = document.createElement ('breakdownCicloConv')
+
+        breakdownCicloConv.innerHTML=`
+        <li class="breakdownhtmlConv"> Precio neto:  ${(netValue*res)}</li>
+        <li class="breakdownhtmlConv"> ${printConvIncCiclo} </li>
+        <li class="breakdownhtmlConv"> ${printConvExcCiclo} </li>
+        <li class="breakdownhtmlConv"> Total por Periodo:  ${(totalExclusiveCiclo*res)} </li>
+        `
+        convertedBreakdown.append(breakdownCicloConv)
+
+        const breakdownTotalConv = document.createElement ('breakdownTotalConv')
+    
+        breakdownTotalConv.innerHTML=`
+        <li class="breakdownhtmlConv"> ${printConvIncTotal} </li>
+        <li class="breakdownhtmlConv"> ${printConvExcTotal} </li>
+        <li class="breakdownhtmlConv"> Total:  ${(totalExclusiveTotal*res)} </li>
+        `
+        convertedBreakdown.append(breakdownTotalConv)
+
+    }
+
     })
 	.catch(err => console.error(err));
-
-
-})
+    const liBreakdownConv = document.querySelectorAll ('.breakdownhtmlConv');
+    liBreakdownConv.forEach (breakdownhtmlConv => {breakdownhtmlConv.remove()});
+  
+})})
